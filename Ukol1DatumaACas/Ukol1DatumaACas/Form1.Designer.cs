c0d5a2c370aa3ec59a890c13782ec242d7cbc6f8
HynekSvoboda <97514770+HynekSvoboda@users.noreply.github.com>
2022-10-18 16:31:07 +0200

    Add files via upload

---
+++ Ukol1DatumaACas/Ukol1DatumaACas/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Ukol1DatumaACas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(31, 62);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(287, 199);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(385, 125);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 60);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Proveď";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Black;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(12, 346);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(68, 34);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "?";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(28, 33);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label1.Size = new System.Drawing.Size(330, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Zadej: např. Hynek;Svoboda;24/05/2005";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(28, 264);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(238, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Žádný člověk nenalezen";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(548, 392);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Ukol";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Ukol1DatumaACas/Ukol1DatumaACas/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ukol1DatumaACas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.CenterToScreen();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime aktualniden = DateTime.Today;
+            int nejstarsi = 0;
+            for (int i = 0; i < textBox1.Lines.Count(); i++)
+            {
+                try
+                {
+                    string text = textBox1.Lines[i];
+                    string[] clovek = text.Split(';');
+                    string[] datum = clovek[2].Split('/');
+                    DateTime date = new DateTime(int.Parse(datum[2]), int.Parse(datum[1]), int.Parse(datum[0]));
+                    TimeSpan rozdil = aktualniden - date;
+
+                    int stari = Convert.ToInt32(rozdil.TotalDays);
+                    if (nejstarsi < stari)
+                    {
+                        label2.Text = "Nejstarsi je: " + clovek[0] + " " + clovek[1];
+                        nejstarsi = stari;
+                    }
+                    else if (nejstarsi == stari)
+                    {
+                        label2.Text += " a " + clovek[0] + " " + clovek[1];
+                        nejstarsi = stari;
+                    }
+                    else label2.Text = "Žádný člověk nenalezen";
+                }
+                catch
+                {
+                    MessageBox.Show("Pravděpodobně jsi zadal špatný formát data, závorky u nadpisu ti pomohou :D nebo jsi nechal prázdný řádek", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("V textBox jsou zapsány osoby ve tvaru jméno;příjmení;datum narození, na každém řádku jedna osoba.\r\nZobrazte nejstaršího člověka.", "INFO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
